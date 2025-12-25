82e9c853546294740caf5bf90f705cf83d919d04
Parth <parth@Parths-MacBook-Air.local>
2025-12-25 13:28:10 +0530

    added other files

---
+++ src/main/java/com/example/challenge/app/ChallengeController.java
@@ -0,0 +1,33 @@
+package com.example.challenge.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class ChallengeController {
+
+    private List<Challenge> challenges=
+            new ArrayList<>();
+
+    public ChallengeController(){
+        Challenge challenge1=
+                new Challenge(1L,"January","learn programming lan");
+        challenges.add(challenge1);
+    }
+
+    @GetMapping("/challenges")
+    public List<Challenge> getAllChallenges(){
+        return challenges;
+    }
+
+    @PostMapping("/challenges")
+    public  String addChallenge(@RequestBody Challenge challenge){
+        challenges.add(challenge);
+        return "challenge added Successfully";
+    }
+}
